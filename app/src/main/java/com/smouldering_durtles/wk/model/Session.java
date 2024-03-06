e95faf8dc6a6b76c834445d10017e6648756798d
Daniel Scherf <munita.gavai@gmail.com>
2024-03-06 15:32:13 +0100

    Add comment for latest changes

--- app/src/main/java/com/smouldering_durtles/wk/model/Session.java
+++ app/src/main/java/com/smouldering_durtles/wk/model/Session.java
@@ -715,6 +715,7 @@ s     *
             }
 
             // Because the user input and subject can contain both Hiragana and Katakana in addition to Kanji, we convert Katakana to Hiragana for the next check
+            // 〜 and the Katakana ヶ have a special meaning, so it's easier to ignore them
             String normalizedSubjectCharacters = KanaUtil.convertKatakanaToHiragana(subject.getCharacters().replaceAll("[〜ヶ]", ""));
             String normalizedAnswer = KanaUtil.convertKatakanaToHiragana(currentAnswer);
 
