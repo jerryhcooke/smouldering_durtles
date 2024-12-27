46899d2ff7051a7dbeb5cee4025c178b2f3c50f9
Jerry Cooke <jerryhcooke@gmail.com>
2024-12-27 13:25:49 +0000

    Re-added Identification.java as f-droid build needs it.

---
+++ app/src/main/java/com/smouldering_durtles/wk/Identification.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright The Smouldering Durtles Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.smouldering_durtles.wk;
+
+
+public final class Identification {
+    /**
+     * The name of the app.
+     */
+    public static final String APP_NAME = "Smouldering Durtles";
+
+    /**
+     * The name of the app as used in a User-Agent HTTP header.
+     */
+    public static final String APP_NAME_UA = "Smouldering Durtles";
+
+    /**
+     * The name of the app as used in URI schemes.
+     */
+    public static final String APP_URI_SCHEME = "smouldering-durtles";
+
+    /**
+     * The name of the author.
+     */
+    public static final String AUTHOR_NAME = "Jerry Cooke";
+
+    /**
+     * The email address of the author.
+     */
+    public static final String AUTHOR_EMAIL = "contact@jhcooke.com";
+
+    /**
+     * The WaniKani username of the author.
+     */
+    public static final String AUTHOR_USERNAME = "joeni";
+
+    /**
+     * Support URL for the app.
+     */
+    public static final String SUPPORT_URL = "https://www.jhcooke.com/sd";
+
+    /**
+     * Store URL for the app.
+     */
+    public static final String STORE_URL = "https://www.jhcooke.com/sd";
+
+    private Identification() {
+        //
+    }
+}
